78199e04145b25f5675958184a48a272f8718793
Georg Jung <gjung@gimas.net>
2024-07-03 13:05:41 +0200

    add demo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using MailKit.Net.Imap;
+
 Console.WriteLine("Hello, World!");
+
+async Task ConnectAsync()
+{
+    using var c = new ImapClient();
+    await c.ConnectAsync("greenmail", 3143, false);
+}
+
+await ConnectAsync(); // this works
+Console.WriteLine("Connected to greenmail");
+
+var s = new ActivitySource("demo");
+s.StartActivity("test");
+Console.WriteLine("Activity started");
+
+await ConnectAsync(); // this doesn't
+Console.WriteLine("Connected to greenmail");
